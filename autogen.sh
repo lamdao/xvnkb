d1563afae87ab8075315ae0fb05b22cd1292ab36
mrchuoi <mrchuoi>
2003-02-26 16:31:08 +0000

    Auto generate configuration files

---
+++ autogen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ln -sf ./config/configure .
+ln -sf ./scripts/install.sh ./install
+ln -sf ./scripts/uninstall.sh ./uninstall
+ln -sf ./scripts/xvnkb.lconf.sh ./localeconf
+ln -sf ../data.c ./tools
+ln -sf ../data.h ./tools
+ln -sf ../debug.h ./tools
+ln -sf ../mode.c ./tools
+ln -sf ../mode.h ./tools
+ln -sf ../property.c ./tools
+ln -sf ../property.h ./tools
+ln -sf ../typedefs.h ./tools
+ln -sf ../xconfig.c ./tools
+ln -sf ../xconfig.h ./tools
