8d8b9a46ccc3fa40fe5e49da5eb59545bdfbcb73
mrchuoi <mrchuoi>
2004-01-12 18:58:01 +0000

    Cleanup & add system checking for LD_PRELOAD, LANG, locale

--- main.c
+++ main.c
@@ -28,6 +28,7 @@
 #include "hotkey.h"
 #include "property.h"
 #include "event.h"
+#include "msgbox.h"
 /*----------------------------------------------------------------------------*/
 void VKCheckMouseStatus()
 {
@@ -80,10 +81,14 @@ void VKRootWindowProcess(XEvent *event, void *data)
 	}
 }
 /*----------------------------------------------------------------------------*/
-void VKMainProcess()
+void VKRootWindowInit()
 {
 	XSelectInput(display, root, PropertyChangeMask|StructureNotifyMask);
 	VKRegisterEvent(root, VKRootWindowProcess, NULL);
+}
+/*----------------------------------------------------------------------------*/
+void VKMainProcess()
+{
 	while( !vk_done ) {
 		usleep(1000);
 
@@ -111,11 +116,105 @@ void VKInitialization()
 	VKStartXSession();
 	VKLoadXResource();
 	VKCreateFlash();
-	VKShowFlash();
 	VKSystrayInit();
 	VKCreateMainWindow();
 	VKCreateMenuWindow();
 	VKCreateHotkeyWindow();
+	VKRootWindowInit();
+}
+/*----------------------------------------------------------------------------*/
+void VKCheckSystem()
+{
+	extern char *sys_lang;
+	static char *core_messages[] = {
+		"The core, xvnkb.so, is not preloaded!\n"
+		"We can not work with Vietnamese properly now.\n"
+		"Please refer to xvnkb manual to fix this problem.",
+		"Phần lõi của xvnkb chưa được nạp!\n"
+		"Chúng ta tạm thời không thể gõ được tiếng Việt.\n"
+		"Hãy tham khảo tài liệu của xvnkb để khắc phục."
+	};
+	static char *lang_messages[] = {
+		"Current value of LANG is \"%s\".\n"
+		"You cannot use Unicode charset to input with this LANG value.\n"
+		"Please set LANG to %s.\n"
+		"Refer to xvnkb manual about setting LANG & create locale.",
+		"Biến môi trường LANG của bạn hiện tại là \"%s\".\n"
+		"Bạn không thể dùng bảng mã Unicode với giá trị LANG này.\n"
+		"Hãy đặt lại LANG thành %s.\n"
+		"Tham khảo thêm tài liệu của xvnkb để chọn LANG và tạo locale."
+	};
+	static char *locale_messages[] = {
+		"Missing locale information for LANG %s in\n"
+		"/usr/share/locale/%s.\n"
+		"We may not work with Vietnamese properly."
+		"Please refer to xvnkb manual to create the information.",
+		"Không tìm thấy các thông tin về bản địa cho LANG %s tại\n"
+		"/usr/share/locale/%s.\n"
+		"Chúng ta có thể sẽ không làm việc được với tiếng Việt.\n"
+		"Tham khảo thêm tài liệu của xvnkb để tạo các thông tin này."
+	};
+
+	FILE *fp;
+	char buf[512];
+	char *ld_preload = getenv("LD_PRELOAD");
+	int core_ok = 0, lang_ok = 0;
+
+	if( ld_preload && strstr(ld_preload, "xvnkb.so") )
+		core_ok = 1;
+
+	if( !core_ok ) {
+		fp = fopen("/etc/ld.so.preload", "r");
+
+		if( fp ) {
+			while( fgets(buf, sizeof(buf), fp) ) {
+				if( strstr(buf, "xvnkb.so") ) {
+					core_ok = 1;
+					break;
+				}
+			}
+			fclose(fp);
+		}
+
+		if( !core_ok )
+			VKShowMessage(core_messages[vk_interface]);
+	}
+
+	if( core_ok ) {
+		if( sys_lang && (strstr(sys_lang, "UTF-8") || strstr(sys_lang, "utf8")) )
+			lang_ok = 1;
+
+		if( !lang_ok ) {
+			char slang[64];
+			if( !sys_lang ) {
+				strcpy(slang, vk_interface ?
+					"vi_VN.UTF-8 hoặc en_US.UTF-8" :
+					"vi_VN.UTF-8 or en_US.UTF-8");
+				sys_lang = "";
+			}
+			else {
+				char *p;
+				strcpy(slang, sys_lang);
+				p = strchr(slang, '.');
+				if( p ) *p = 0;
+				strcat(slang, ".UTF-8");
+			}
+			sprintf(buf, lang_messages[vk_interface], sys_lang, slang);
+			VKShowMessage(buf);
+		}
+		else {
+			sprintf(buf, "/usr/share/locale/%s/LC_IDENTIFICATION", sys_lang);
+			fp = fopen(buf, "r");
+			if( fp ) {
+				fclose(fp);
+				VKShowFlash();
+			}
+			else {
+				sprintf(buf, locale_messages[vk_interface], sys_lang, sys_lang);
+				VKShowMessage(buf);
+			}
+		}
+	}
 }
 /*----------------------------------------------------------------------------*/
 void VKFinalization()
@@ -132,6 +231,7 @@ int main(int argc, char **argv)
 {
 	VKLoadConfig(argc, argv);
 	VKInitialization();
+	VKCheckSystem();
 	VKMainProcess();
 	VKFinalization();
 	VKSaveConfig();
